d72bdf6383e4d581b69cf5a5d9673a436e64b4db
Glenn Dirkx <glenn.dirkx@gmail.com>
2025-12-03 21:35:51 +0100

    add memtest cuz it's nice

--- modules/systemd-boot.nix
+++ modules/systemd-boot.nix
@@ -6,4 +6,6 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.loader.systemd-boot.memtest86.enable = true;
 }
