ca6a45de8d0b4eac824ce044ff3426b9cc8718ab
Glenn Dirkx <glenn.dirkx@gmail.com>
2026-08-02 22:22:00 +0200

    Revert "try out host level values"
    This reverts commit b49f1a875094bb90e328171a0883f2601f63cab3.

--- modules/k3s.nix
+++ modules/k3s.nix
@@ -276,11 +276,6 @@ in
                 repoURL = "https://github.com/Fluufff/infra-argocd.git";
                 targetRevision = "next";
                 path = "applications";
-                helm = {
-                  valueFiles = [
-                    ("hosts/" + name + "/values.yaml")
-                  ];
-                };
               };
 
               destination = {
